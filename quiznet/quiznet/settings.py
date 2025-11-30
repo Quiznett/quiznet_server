eb6fcf61e0d8e79f7867e76c503e9ee3dccce336
ashvin-mca2025 <ashvink.mca25@cs.du.ac.in>
2025-11-30 15:03:23 +0530

    Change SESSION_COOKIE_SECURE to False
    Set SESSION_COOKIE_SECURE to False for development.

--- quiznet/quiznet/settings.py
+++ quiznet/quiznet/settings.py
@@ -186,7 +186,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
 }
 
-SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 # CSRF_COOKIE_HTTPONLY typically False (so frontend can read CSRF if you use double-submit)
